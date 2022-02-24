bc1f3a0303ac4d492c699e4f5dbef378c9f56ad1
Taiwo Oyeniyi <muyiwaoyeniyi@yahoo.com>
2022-02-23 19:24:01 -0500

    m02w5

--- flex-m01w4/code.js
+++ flex-m01w4/code.js
@@ -1,28 +1,21 @@
-const fs = require("fs");
-const EventEmitter = require("events");
+let counter = 0;
 
-const fileEvents = new EventEmitter();
+// create a handle
+// const intervalHandle =
 
-const name = process.argv[2];
+setInterval((item) => {
+  counter++;
+  console.log(e);
+  console.log("Counter: ", counter);
+}, 1000);
 
-console.log("Writing to file");
+// const callback = function () {
+//   counter++;
+//   console.log("Counter: ", counter);
+// };
 
-for (let i = 0; i < 1; i++) {
-  fs.writeFileSync("./name.txt", `${name} \n`, { flag: "a+" });
-}
+// setInterval(callback, 1000);
 
-fs.readFile("./name.txt", { encoding: "utf-8" }, (err, data) => {
-  fileEvents.emit("fileRead");
-  fileEvents.emit("READY!");
-  console.log("file data ...", data);
-});
-
-// fileEvents.on("fileRead", () => {});
-
-// fileEvents.on("fileRead", () => console.log("file is read 2"));
-
-// fileEvents.on("fileRead", () => console.log("file is read 3"));
-
-fileEvents.on("READY!", () => console.log("READY!!!"));
-
-console.log("moving on ....");
+// [1, 2, 3].forEach((item) => {
+//   console.log(item);
+// });
